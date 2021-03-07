46b0db488dda75a71ecc4145f7ac67524d2c3209
Szymon Reiter <reiter.szymon@gmail.com>
2021-03-07 22:40:25 +0100

    initial stuff

---
+++ src/main/java/pl/zimi/zadanka/X.java
@@ -0,0 +1,13 @@
+package pl.zimi.zadanka;
+
+public class X {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public String ble(String a) {
+        return a;
+    }
+}
---
+++ src/test/java/pl/zimi/zadanka/XTest.java
@@ -0,0 +1,11 @@
+package pl.zimi.zadanka;
+
+import org.junit.jupiter.api.Test;
+
+public class XTest {
+
+    @Test
+    void jakisTest() {
+
+    }
+}
