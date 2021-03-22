37ffbfc161b85088efce29d1473369da46c56ced
sm000k <smok.lacki@gmail.com>
2021-03-22 02:25:04 +0100

    Zad 6 obsługa testu [1,1,2] i [1,1,1,1,1,1,2]

--- src/main/java/pl/zimi/zadanka/Zad6.java
+++ src/main/java/pl/zimi/zadanka/Zad6.java
@@ -1,5 +1,29 @@
 package pl.zimi.zadanka;
 
 public class Zad6 {
+    public int removeDuplicates(int[] nums) {
+        int Len = 0;
+        int numsLen = nums.length - 1;
+        int arrayRank = nums[numsLen];
 
+
+        int i = 0;
+        while (nums[arrayRank - 1] != arrayRank) {
+            if (i + 1 == nums.length) i = 0;
+            if (nums[i] - nums[i + 1] == 0) {
+                System.out.println("przerzut =" + "i[" + i + "] " + nums[i]);
+                for (int j = i + 1; j < numsLen; j++) {
+                    nums[j] = nums[j + 1];
+                }
+
+            }
+            i++;
+        }
+        for (int number : nums) {
+            System.out.println(number);
+        }
+
+
+        return arrayRank;
+    }
 }
--- src/test/java/pl/zimi/zadanka/Zad6Test.java
+++ src/test/java/pl/zimi/zadanka/Zad6Test.java
@@ -1,10 +1,22 @@
 package pl.zimi.zadanka;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class Zad6Test {
     @Test
     void templateTest(){
 
     }
-
+    @Test
+    void simpleOneOneTwo(){
+        Zad6 object= new Zad6();
+        int[] nums = {1,1,2};
+        Assertions.assertEquals(2,object.removeDuplicates(nums));
+    }
+    @Test
+    void simplelong(){
+        Zad6 object= new Zad6();
+        int[] nums = {1,1,1,1,1,1,2};
+        Assertions.assertEquals(2,object.removeDuplicates(nums));
+    }
 }
